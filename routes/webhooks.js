2ef349a039f684ab80bd52a1a2fa84e4fdab1b6b
Enrique Garcia Navalon <garcianavalon@gmail.com>
2016-10-11 12:37:30 +0200

    basic webhook setup

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -4,7 +4,9 @@ var router = express.Router();
 /* CONFIGURE YOUR WEBHOOKS HERE */
 // NOTE(garcianavalon) they get prefixed with /webhooks/ in app.js
 router.post('/test', function(req, res, next) {
-  // TODO(garcianavalon) console.log something
+  console.log('Received a POST to /webhooks/test with payload ', req.body);
+  // just return the payload to check everything is ok
+  res.json(req.body);
 });
 
 module.exports = router;
---
+++ test/webhooks.test.js
@@ -0,0 +1,29 @@
+const webhooks = require('../routes/webhooks.js');
+const reqres = require('reqres');
+const chai = require('chai');
+const should = chai.should();
+chai.use(require('sinon-chai'));
+
+describe('webhooks.js', function() {
+
+  let req, res;
+
+  describe('/test', function() {
+
+    beforeEach(function () {
+      req = reqres.req({ method: 'POST', url: '/test', body: { event: 'mocha test!' } });
+      res = reqres.res();
+    });
+
+    it('works', function(done){
+      webhooks.handle(req, res);
+      res.on('end', function () {
+        res.json.should.have.been.calledWithExactly({ event: 'mocha test!' });
+        done();
+      });
+
+    });
+
+  });
+
+});
